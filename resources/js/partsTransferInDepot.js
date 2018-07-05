59e7316ef83126e5fde4f04f00668ddc6f486964
聂亚兰 <3242662985@qq.com>
2018-07-05 15:31:43 +0800

    bugfix

---
+++ resources/js/partsTransferInDepot.js
@@ -0,0 +1,29 @@
+define(['angular', 'jquery', 'lodash', 'mock', 'httpMethod', 'ngCommonModule', 'ui-bootstrap-tpls', 'angular-animate', 'angular-sanitize', 'ngStorage'], function (angular, $, _, Mock) {
+    angular
+        .module('terminalCollectionModule', ['ngCommonModule', 'httpMethod'])
+        .run(['$rootScope', function ($rootScope) {
+            $rootScope.termRecoverBaseInfo = null; //获取终端替换页面基本信息
+            $rootScope.allotItemList = []; //待提交的终端列表
+        }])
+        .filter('prodTypeFilter', function () {
+            return function (val) {
+                switch (val) {
+                    case '1':
+                        return '宽带';
+                        break;
+                    case '2':
+                        return 'IPTV';
+                        break;
+                }
+            }
+        })
+        .controller('partsTransferCtrl', ['$scope', '$rootScope', '$uibModal', 'httpMethod', 'JqueryDialog', function ($scope, $rootScope, $uibModal, httpMethod, JqueryDialog) {
+        
+        }])
+        .controller('terminalCodeCtrl', ['$scope', '$rootScope', '$uibModal', 'httpMethod', 'JqueryDialog', function ($scope, $rootScope, $uibModal, httpMethod, JqueryDialog) {
+        
+        }])
+        .controller('productQueryCodeCtrl', ['$scope', '$rootScope', '$uibModal', 'httpMethod', 'JqueryDialog', function ($scope, $rootScope, $uibModal, httpMethod, JqueryDialog) {
+        
+        }])
+});
