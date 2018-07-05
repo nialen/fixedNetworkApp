bf9d257a72a2bd2d6f13e9e2e02ab51bf809e39c
聂亚兰 <3242662985@qq.com>
2018-07-05 16:59:31 +0800

    bugfix

--- resources/js/common/requireConfig.js
+++ resources/js/common/requireConfig.js
@@ -8,9 +8,10 @@ require.config({
 		'angular': './angular.min',
 		'angular-touch': './angular-touch.min',
 		'angular-animate': './angular-animate.min',
-		'angular-sanitize': './angular-sanitize.min',
+		'angular-sanitize': './angular-sanitize.min',		
 		'jquery': './jquery.min',
 		'jqueryDialog': './jquery.dialog',
+		'ui-bootstrap-tpls':'./ui-bootstrap-tpls-2.1.3',
 		'angular-locale_zh-cn': './angular-locale_zh-cn',
 		'lodash': './lodash',
 		'mock': './mock',
@@ -18,8 +19,10 @@ require.config({
 		'ngStorage': './ngStorage',
 		'httpServer': './httpServer',
 		'httpMethod': './httpMethod',
+		'moment': './moment',
 		'ngCommonModule': './ngCommonModule',
 		'datepicker': './datepicker',
+		'iscroll':'./iscroll'
 	},
 	shim: {
 		'angular': {
@@ -36,7 +39,10 @@ require.config({
 		'angular-sanitize': {
 			deps: ['angular'],
 			exports: 'angular-sanitize'
-		},		
+		},	
+		'ui-bootstrap-tpls': {
+			deps: ['angular']
+		},	
 		'angular-locale_zh-cn': {
 			deps: ['angular']
 		},
@@ -50,14 +56,18 @@ require.config({
 		'httpMethod': {
 			deps: ['angular', 'httpServer'],
 			exports: 'httpMethod'
-		},		
+		},	
+		'moment': {
+			init: function (moment) {
+				return moment;
+			}
+		},	
 		'ngCommonModule': {
 			deps: ['angular', 'jqueryDialog'],
 			exports: 'ngCommonModule'
 		},
 		// 'datepicker': {
-		// 	deps: ['angular', 'jqueryDialog'],
-		// 	exports: 'ngCommonModule'
+		// 	deps: ['angular'],
 		// }
 	}
 });
