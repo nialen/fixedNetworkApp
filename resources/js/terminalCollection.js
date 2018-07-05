5844d6b2774aa560ea83f25180b8a4709a720f86
聂亚兰 <3242662985@qq.com>
2018-07-05 09:46:32 +0800

    bugfix

--- resources/js/terminalCollection.js
+++ resources/js/terminalCollection.js
@@ -18,51 +18,9 @@ define(['angular', 'jquery', 'lodash', 'mock', 'httpMethod', 'ngCommonModule', '
             }
         })
         .controller('terminalCollectionCtrl', ['$scope', '$rootScope', '$uibModal', 'httpMethod', 'JqueryDialog', function ($scope, $rootScope, $uibModal, httpMethod, JqueryDialog) {
-            // httpMethod.qryTermTakeBaseInfo().then(function (rsp) {
-            //     if (rsp.success) {
-            //         $rootScope.termRecoverBaseInfo = rsp.data; //获取拆机终端收取页面基本信息
-            //     }
-            // });
-
-            $scope.operateTypeList = [{
-                operateTypeId: '1',
-                operateTypeName: '上客户门收取'
-            }, {
-                operateTypeId: '2',
-                operateTypeName: '营业厅收取'
-            }]; //收取方式列表
-
-            $scope.operateType = {
-                operateTypeId: '1',
-                operateTypeName: '上客户门收取'
-            };
-
-            $scope.processList = [{
-                processType: '1',
-                processName: '维修',
-            },{
-                processType: '2',
-                processName: '翻新',
-            },{
-                processType: '4',
-                processName: '直接二次入库',
-            },{
-                processType: '5',
-                processName: '报废',
-            }];
-
-
-            //从待提交列表中删除终端
-            $scope.delTerminal = function (index) {
-                $rootScope.allotItemList.splice(index, 1);
-            };
-            //提交
-            $scope.termTakeOrderSubmit = function () {
-
-            };
-
-            $scope.cancelTermTakeOrder = function(){
-                window.location.reload();
-            }
+        
+        }])
+        .controller('terminalCodeCtrl', ['$scope', '$rootScope', '$uibModal', 'httpMethod', 'JqueryDialog', function ($scope, $rootScope, $uibModal, httpMethod, JqueryDialog) {
+        
         }])
 });
